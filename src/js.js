0324dc86d755e74281ce21a4d79dd02cc0b8e831
Nagornykh Maria <manyaknag@gmail.com>
2021-08-02 20:25:34 +0300

    ko 2

--- src/js.js
+++ src/js.js
@@ -1,7 +1,11 @@
 // This is a simple *viewmodel* - JavaScript that defines the data and behavior of your UI
 function AppViewModel() {
-  this.firstName = "Маша";
-  this.lastName = "Наг";
+  this.firstName = ko.observable("Маша");
+  this.lastName = ko.observable("Наг");
+
+  this.fullName = ko.computed(function () {
+    return this.lastName() + " " + this.firstName();
+  }, this);
 }
 
 // Activates knockout.js
